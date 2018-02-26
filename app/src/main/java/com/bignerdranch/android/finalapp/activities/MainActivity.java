e4337f8e229a8dc3bc39e17d19cecaf1f8cd169d
jemtca <30645648+jemtca@users.noreply.github.com>
2018-02-25 21:13:27 -0800

    Android Programming

--- app/src/main/java/com/bignerdranch/android/finalapp/activities/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/finalapp/activities/MainActivity.java
@@ -1,33 +1,15 @@
 package com.bignerdranch.android.finalapp.activities;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import com.bignerdranch.android.finalapp.R;
 import com.bignerdranch.android.finalapp.fragments.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected Fragment createFragment(){
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
-
-        //getting the fragment manager
-        FragmentManager fm = getSupportFragmentManager();
-        //retrieve the fragment from the FragmentManager (ask for the container view id)
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container_first_screen);
-
-        if(fragment == null){
-
-            fragment = new MainFragment();
-            //create a new fragment transaction, include one add operation and then commit it
-            fm.beginTransaction().add(R.id.fragment_container_first_screen, fragment).commit();
-
-        }
+        return new MainFragment();
 
     }
 
--- app/src/main/java/com/bignerdranch/android/finalapp/activities/SecondActivity.java
+++ app/src/main/java/com/bignerdranch/android/finalapp/activities/SecondActivity.java
@@ -1,8 +1,17 @@
 package com.bignerdranch.android.finalapp.activities;
 
 
+import android.support.v4.app.Fragment;
+
+import com.bignerdranch.android.finalapp.fragments.SecondFragment;
+
 public class SecondActivity extends SingleFragmentActivity {
 
+    @Override
+    protected Fragment createFragment(){
 
+        return new SecondFragment();
+
+    }
 
 }
